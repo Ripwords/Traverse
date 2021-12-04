f93a28a4c52a9e0c5481f1de5337925c3d33071e
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-12-04 23:38:14 +0800

    added always on top button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { appWindow } from '@tauri-apps/api/window'
 
 appWindow.setDecorations(true)
 
+const alwaysTop = ref(false)
 const opacity = ref(50)
 const fontSize = ref(26)
 const text = ref<any>(null)
@@ -13,6 +14,11 @@ const themeColor = ref(0)
 const changeTheme = () => {
   themeColor.value = theme.value === 'dark' ? 255 : 0
   theme.value = theme.value === 'dark' ? 'light' : 'dark'
+}
+
+const changeAlwaysTop = () => {
+  alwaysTop.value = !alwaysTop.value
+  appWindow.setAlwaysOnTop(alwaysTop.value)
 }
 
 onMounted(() => {
@@ -41,8 +47,24 @@ watch(fontSize, () => {
       <n-h2 class="text-dark-900 dark:text-light-50">Font Size</n-h2>
       <n-slider v-model:value="fontSize" :step="1"></n-slider>
     </div>
-    <i-ri:settings-3-line class="absolute right-10px bottom-15px text-size-35px text-dark-900 dark:text-light-50" @click="settings = !settings"></i-ri:settings-3-line>
-    <i-mdi:theme-light-dark class="absolute right-10px bottom-75px text-size-35px text-dark-900 dark:text-light-50" @click="changeTheme()"></i-mdi:theme-light-dark>
+    <n-tooltip placement="top-start" trigger="hover">
+      <template #trigger>
+        <i-ri:settings-3-line class="absolute right-10px bottom-15px text-size-35px text-dark-900 dark:text-light-50" @click="settings = !settings" />
+      </template>
+      Settings
+    </n-tooltip>
+    <n-tooltip placement="top-start" trigger="hover">
+      <template #trigger>
+        <i-mdi:theme-light-dark class="absolute right-10px bottom-75px text-size-35px text-dark-900 dark:text-light-50" @click="changeTheme()" />
+      </template>
+      Theme
+    </n-tooltip>
+    <n-tooltip placement="top-start" trigger="hover">
+      <template #trigger>
+        <i-mdi:arrow-top-left-thin-circle-outline class="absolute right-10px bottom-135px text-size-35px text-dark-900 dark:text-light-50" @click="changeAlwaysTop()" />
+      </template>
+      Always-on-top
+    </n-tooltip>
     <textarea class="text-dark-900 dark:text-light-50 w-100vw h-100vh p-5" ref="text" spellcheck="false" @click="settings = false"></textarea>
   </div>
 </template>
